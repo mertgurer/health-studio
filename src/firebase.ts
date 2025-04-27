b3bfda8a776df4dad9a79734053cd52201552d68
Mert Gürer <mert.gurer@hotmail.com.tr>
2025-04-27 18:08:25 +0300

    update

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,11 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getDownloadURL, getStorage } from "firebase/storage";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getDownloadURL, getStorage } from "firebase/storage";
 
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG || "");
+// const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG || "");
 
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-const db = getFirestore(app);
+// const app = initializeApp(firebaseConfig);
+// const storage = getStorage(app);
+// const db = getFirestore(app);
 
-export { storage, db, getDownloadURL };
+// export { storage, db, getDownloadURL };
