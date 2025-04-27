d6e5a497829f79eb4cc75300f90d50868ec6aa55
Mert Gürer <mert.gurer@hotmail.com.tr>
2025-04-27 19:13:38 +0300

    update

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -7,7 +7,9 @@ if (!getApps().length) {
         credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            privateKey: JSON.parse(
+                process.env.FIREBASE_PRIVATE_KEY || ""
+            ).privateKey?.replace(/\\n/g, "\n"),
         }),
     });
 }
