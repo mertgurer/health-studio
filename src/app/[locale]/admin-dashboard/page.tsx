aeaf8b8e8a1f4d2f871be0e86e9be29db705bc40
Mert Gürer <mert.gurer@hotmail.com.tr>
2025-04-27 23:41:04 +0300

    update

--- src/app/[locale]/admin-dashboard/page.tsx
+++ src/app/[locale]/admin-dashboard/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Dashboard() {
-    return <div>Dashboard</div>;
+    return <div className="flex flex-col py-20 px-28">Dashboard</div>;
 }
 
 export default Dashboard;
