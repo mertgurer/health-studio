dca5d7f2a4a6a29799a646d5380bf399e62942c5
Mert Gürer <mert.gurer@hotmail.com.tr>
2025-04-29 21:53:55 +0300

    Token verification error

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -46,7 +46,7 @@ export async function GET(request: NextRequest) {
             return NextResponse.json(true, { status: 200 });
         }
     } catch (error) {
-        console.error("Token verification error");
+        console.error("Token verification error:", error);
     }
 
     return NextResponse.json(false, { status: 401 });
